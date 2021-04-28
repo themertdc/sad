573546649d9120de6980566423189d37a4379971
Glitch (quilted-hyper-toaster) <none>
2021-04-28 20:35:11 +0000

    🏊🏼🎑 Updated with Glitch

--- events/disconnect.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = client => {
-  console.log(`Bağlantı Koptu! ${new Date()}`);
-};
-//x_REDDAWN_x#0001
--- events/reconnecting.js
+++
@@ -1,4 +0,0 @@
-module.exports = client => {
-  console.log(`Başlatılıyor... ${new Date()}`);
-};//x_REDDAWN_x#0001
-//x_REDDAWN_x#0001
